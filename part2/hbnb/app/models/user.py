2d65d48642c3040302c1779431c9ff2cad50aed4
Krapaud <mur.mickael@gmail.com>
2025-10-10 14:52:02 +0200

    refactor: remove password parameter from UserModel constructor and improve validation logic

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -2,15 +2,20 @@ from . import BaseModel
 import re
 
 class UserModel(BaseModel):
-    def __init__(self, first_name, last_name, email, password, is_admin=False):
+    def __init__(self, first_name, last_name, email, is_admin=False):
         super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.is_admin = is_admin
+        self.validate_user_data()
     
     def validate_user_data(self):
         """Validate user data"""
-        if not self.first_name and len(first_name) > 50:
-            raise ValueError("First name is required")
-        if not self.last_name and len(last_name) > 50:
-            raise ValueError("Last name is required")
+        if not self.first_name or len(self.first_name) > 50:
+            raise ValueError("First name is required and must be <= 50 characters")
+        if not self.last_name or len(self.last_name) > 50:
+            raise ValueError("Last name is required and must be <= 50 characters")
         if not self.email or not self.is_valid_email(self.email):
             raise ValueError("Valid email is required")
     
@@ -18,9 +23,3 @@ class UserModel(BaseModel):
         """Basic email validation"""
         email_pattern = r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
         return re.match(email_pattern, email) is not None
-
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.is_admin = is_admin
-        self.validate_user_data()
