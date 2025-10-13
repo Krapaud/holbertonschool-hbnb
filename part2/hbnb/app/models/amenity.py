1d2d03578539d3657a8ee210813430724032674f
Krapaud <mur.mickael@gmail.com>
2025-10-13 11:51:38 +0200

    refactor: remove placeholder methods for amenity management in AmenityModel and HBnBFacade

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -8,19 +8,3 @@ class AmenityModel(BaseModel):
             self.name = name
         else:
             raise ValueError ("Required, maximum length of 50 characters.")
-
-    def create_amenity(self, amenity_data):
-    # Placeholder for logic to create an amenity
-        pass
-
-    def get_amenity(self, amenity_id):
-        # Placeholder for logic to retrieve an amenity by ID
-        pass
-
-    def get_all_amenities(self):
-        # Placeholder for logic to retrieve all amenities
-        pass
-
-    def update_amenity(self, amenity_id, amenity_data):
-        # Placeholder for logic to update an amenity
-        pass
--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -23,3 +23,19 @@ class HBnBFacade:
         user.update(user_data)
         user.validate_user_data()
         return user
+
+    def create_amenity(self, amenity_data):
+    # Placeholder for logic to create an amenity
+        pass
+
+    def get_amenity(self, amenity_id):
+        # Placeholder for logic to retrieve an amenity by ID
+        pass
+
+    def get_all_amenities(self):
+        # Placeholder for logic to retrieve all amenities
+        pass
+
+    def update_amenity(self, amenity_id, amenity_data):
+        # Placeholder for logic to update an amenity
+        pass
