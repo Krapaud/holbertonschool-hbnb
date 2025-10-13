da12cf7cfcc8ae287c0027b4933cd7288c62af23
Krapaud <mur.mickael@gmail.com>
2025-10-13 14:56:20 +0200

    Add review API endpoints and update facade with review/place repositories

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,11 +1,16 @@
-from app.persistence.repository import InMemoryRepository
-from app.models.user import UserModel
 from app.models.amenity import AmenityModel
+from app.models.place import PlaceModel
+from app.models.review import ReviewModel
+from app.models.user import UserModel
+from app.persistence.repository import InMemoryRepository
+
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
+        self.review_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
 
     def create_user(self, user_data):
         user = UserModel(**user_data)
