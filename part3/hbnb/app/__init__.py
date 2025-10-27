c010f589bcb89930fd2516ec3598994e61c94cdd
Krapaud <mur.mickael@gmail.com>
2025-10-27 13:59:42 +0100

    feat: Add configurable app initialization to create_app function

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -6,8 +6,9 @@ from flask import Flask, jsonify
 from flask_restx import Api
 
 
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
+    app.config.from_object(config_class)
     api = Api(
         app,
         version='1.0',
