834bd17992a189d13abb989d1b1cb57f61318955
Krapaud <mur.mickael@gmail.com>
2025-10-27 14:01:24 +0100

    feat: Initialize Bcrypt in create_app for password management

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -4,11 +4,17 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
 from flask import Flask, jsonify
 from flask_restx import Api
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    
+    bcrypt.init_app(app)
+    
     api = Api(
         app,
         version='1.0',
